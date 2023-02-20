336a2d8e5d2c80ecf31332f91f9eb24f9a12ca63
Sangay Thinley <thinley.sangay@selise.ch>
2023-02-20 13:44:46 +0600

    nextjs setup and adding new pages

---
+++ pages/index.js
@@ -0,0 +1,6 @@
+//domain.com
+function HomePage() {
+    return <h1>The Home Page</h1>
+}
+
+export default HomePage;
---
+++ pages/news/detail/[detailId].js
@@ -0,0 +1,6 @@
+// domain.com/news/detail/some-thing-random-id
+function DetailPage() {
+    return <h1>The Detail Page</h1>
+}
+
+export default DetailPage;
