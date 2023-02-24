28c4df62de7958b5ae3abe06ce713300412366a1
Sangay Thinley <thinley.sangay@selise.ch>
2023-02-24 15:55:39 +0600

    fixed fallback issues

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -35,7 +35,7 @@ export async function getStaticPaths(filter, options) {
     await client.close();
 
     return {
-        fallback: false,
+        fallback: 'blocking',
         paths: meetups.map(meetup => ({
             params: {meetupId: meetup._id.toString()}
         }))
